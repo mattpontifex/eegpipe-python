dcc1d8ecd02d464b41c5c2594da8c02024ce5fa8
Matthew B. Pontifex <31024683+mattpontifex@users.noreply.github.com>
2020-12-27 20:29:04 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    #https://uoftcoders.github.io/studyGroup/lessons/python/packages/lesson/
+    
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='eegpipe',
+    url='https://github.com/mattpontifex/eegpipe',
+    author='Matthew B. Pontifex',
+    author_email='pontifex@msu.edu',
+    # Needed to actually package something
+    packages=['eegpipe'],
+    # Needed for dependencies
+    install_requires=['numpy', 'scipy', 'matplotlib'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='A python package for rapidly processing EEG data.',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+    
+    classifiers=['Intended Audience :: Science/Research',
+       'Programming Language :: Python',
+       'Topic :: Scientific/Engineering',
+       'Operating System :: Microsoft :: Windows',
+       'Operating System :: MacOS',
+       'Programming Language :: Python :: 3',
+       ],
+    keywords='neuroscience neuroimaging EEG brain',
+)
